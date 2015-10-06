7f5b47c68e2b719caa9874f9e7693ea560695249
Zsolt Imre <imrexzsolt@gmail.com>
2015-10-06 17:26:50 +0000

    Updated setup.sh and requirements.txt for the webserver

--- webserver/setup.sh
+++ webserver/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+
+#!/bin/bash
+
+# Essentials
 apt-get update
 apt-get upgrade
-apt-get install python-pip python-dev
+apt-get install python-pip python-sqlite python-dev libffi-dev libxml2-dev libxslt1-dev lib32z1-dev libssl-dev
 pip install -r requirements.txt
 
 mkdir ./etc/ssl
