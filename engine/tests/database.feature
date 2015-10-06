851945e94a2f462fb8b2a4695b811460aaaccb9f
Zsolt Imre <imrexzsolt@gmail.com>
2015-10-06 05:53:08 +0000

    Added more test cases to the database handler class test.

--- engine/tests/database.feature
+++ engine/tests/database.feature
@@ -1,5 +1,20 @@
 Feature: saving and loading issue data
 
+  Scenario: no issues in database
+      Given we have root and config and no issues
+       When we load the list of issues
+       Then we receive a list without issues
+
+  Scenario: no issues in database
+      Given we have root and config and no issues
+       When we load an issue
+       Then we receive an empty dictionary without issue data
+
+  Scenario: no issues in database
+      Given we have root and config and no issues
+       When we delete an issue
+       Then we receive False as the issue was not found
+
   Scenario: save an issue to the database
       Given we have root, config and issue data
        When we save an issue
@@ -13,7 +28,7 @@ Feature: saving and loading issue data
   Scenario: get an issue from database
       Given we have root directory and configuration
        When we load an issue
-       Then we receive a dictionary
+       Then we receive a dictionary with issue data
 
   Scenario: delete an issue from database
       Given we have root directory and configuration
--- engine/tests/steps/database.py
+++ engine/tests/steps/database.py
@@ -13,6 +13,13 @@ sys.path.append(ROOT_DIR + "/../../classes")
 from ConfigurationHandler import ConfigurationHandler
 from DatabaseHandler import DatabaseHandler
 
+@given('we have root and config and no issues')
+def step_impl(context):
+    assert os.path.isfile(ROOT_DIR + "/../../etc/engine.config")
+    context.root        = ROOT_DIR + "/../../"
+    config_file         = ROOT_DIR + "/../../etc/engine.config"
+    context.config_data = ConfigurationHandler(config_file).get()
+
 @given('we have root, config and issue data')
 def step_impl(context):
     assert os.path.isfile(ROOT_DIR + "/../../etc/engine.config")
@@ -49,6 +56,11 @@ def step_impl(context):
     context.database   = DatabaseHandler(context.config_data, context.root)
     context.return_val = context.database.loadIssues()
 
+@then('we receive a list without issues')
+def step_impl(context):
+    assert type(context.return_val) == list
+    assert context.return_val == []
+
 @then('we receive a list of dictionaries')
 def step_impl(context):
     assert type(context.return_val) == list
@@ -60,14 +72,25 @@ def step_impl(context):
     context.database   = DatabaseHandler(context.config_data, context.root)
     context.return_val = context.database.loadIssue(1)
 
-@then('we receive a dictionary')
+@then('we receive an empty dictionary without issue data')
 def step_impl(context):
     assert type(context.return_val) == dict
+    assert context.return_val == {}
+
+@then('we receive a dictionary with issue data')
+def step_impl(context):
+    assert type(context.return_val) == dict
+    assert context.return_val.get('job_id')
+    assert context.return_val.get('payload')
 
 @when('we delete an issue')
 def step_impl(context):
     context.database   = DatabaseHandler(context.config_data, context.root)
     context.return_val = context.database.deleteIssue(1)
+
+@then('we receive False as the issue was not found')
+def step_impl(context):
+    assert context.return_val == False
 
 @then('we receive True if issue was deleted')
 def step_impl(context):
